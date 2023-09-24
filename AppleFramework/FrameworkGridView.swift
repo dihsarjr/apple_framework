8ea23573badad96dfdf725bf976711a39d5977a6
dihsarjr <mohammed1rashid786@gmail.com>
2023-09-24 12:09:03 +0530

    navigation_view_added

---
+++ AppleFramework/FrameworkGridView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkGridView.swift
+//  AppleFramework
+//
+//  Created by Mohammed Rashid on 24/09/23.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+}
