e1b29249c9f9c2508cddf6d02101455995705580
dihsarjr <mohammed1rashid786@gmail.com>
2023-09-24 14:04:15 +0530

    detail_view

---
+++ AppleFramework/FrameworkDetailedView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkDetailedView.swift
+//  AppleFramework
+//
+//  Created by Mohammed Rashid on 24/09/23.
+//
+
+import SwiftUI
+
+struct FrameworkDetailedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkDetailedView()
+}
